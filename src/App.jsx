fb8aa59e960a2752d0120a78e2fe97d3e4e6c853
barbalusa <antonkravchenkooutlook.com>
2023-04-11 19:29:55 +0300

    eu

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Header } from './components/Header/Header';
 import Main from './components/Main/Main';
 
 
-function App() {
+faunction App() {
 const [count, setCount] = useState(0);
 
   
