2e55e149b15fd51fd1e3e9169f0edd3d13a2f313
barbalusa <antonkravchenkooutlook.com>
2023-05-04 13:17:10 +0300

    finish_lesson_8

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,19 @@
 
 // import { useState } from 'react';
-import { useEffect, useState, useCallback } from 'react';
 import './App.css';
 import { MemoFooter } from './components/Footer/Footer';
 import { MemoHeader } from './components/Header/Header';
 import {Main} from './components/Main/Main';
-import { v4 as uuidv4 } from 'uuid';
-import { LS_TOKEN } from './utils/constants';
+import { useTodos } from './hooks/useTodos';
 
-const getDataFromLC = () => {
-  const todoList = localStorage.getItem(LS_TOKEN)
-//   if (todoList) {
-//     return JSON.parse(todoList)
-//   } else {
-//     return []
-//   }
-return todoList ? JSON.parse(todoList) : []
 
-}
+
 
 function App() {
 
-const [todos, setTodos] = useState(getDataFromLC())
+const {todos, addToList, deleteList, deleteOneTodo, updTodoStatus} = useTodos()
 
-useEffect(() => {
- localStorage.setItem(LS_TOKEN, JSON.stringify(todos))
-}, [todos])
-
-
-const addToList = useCallback((value) => {
- 
-  const newTodo = {
-      id: uuidv4(),
-      title: value,
-      status: false
-}
-
-  setTodos((prev)=>[newTodo, ...prev])
-
-},[])
-
-const deleteList = () => {
-setTodos([])
-}
-
-const deleteOneTodo = (id) => {
-setTodos((prev) => prev.filter((todo)=>todo.id !== id))
-}
-
-const updTodoStatus = (id) => {
-  setTodos((prev) => prev.map(todo => {
-    if (id === todo.id) return {
-      ...todo,
-      status: !todo.status
-    }
-    return todo;
-
-  }))
-}
-
-
-  return (
+return (
     <div className="App">
       <MemoHeader addToList={addToList}/>
    
